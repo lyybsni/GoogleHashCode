e8109ad92b810cb31334563a502f1abedbd428eb
LI Yuyang <lyybsni@gmail.com>
2019-01-04 10:21:59 +0800

    I forget how to read files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
+/**
+ * Disclaimer: This project is for Google Hash Code Compitition 2019 Practice Program
+ * Owned by LI Yuyang & XIE Zhilin
+ */
+
+#include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    cout << "?";
+
+    char path[60];
+    cin.getline(path, 60);
+
+    long row, column, limit, maximum;
+
+    cout << "?";
+
+    FILE* file = fopen(path, "r");
+    
+    fprintf(file, "%s", "s");
+
+    if(file == NULL){
+        cout << "Error" << endl;
+        return 0;
+    }
+
+    cout << "?";
+
+    while(fscanf(file,"%d%d%d%d", row, column, limit, maximum)){
+        cout << row << column << limit << maximum << endl;
+    }
+
     return 0;
 }
